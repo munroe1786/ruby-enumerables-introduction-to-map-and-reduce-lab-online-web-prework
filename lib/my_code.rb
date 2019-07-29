82c7ad9497ddd453f298c661d4a5ba80cb1235f9
Eliza Munroe <era3munroe@gmail.com>
2019-07-29 11:45:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,81 @@
-# My Code here....
+def map_to_negativize(source_array)
+  counter = 0
+  new_array = [ ]
+  while source_array[counter] do
+    new_array.push(source_array[counter] *-1)
+    counter += 1
+  end
+  return new_array
+end
+
+def map_to_no_change(source_array)
+  counter = 0
+  new_array = [ ]
+  while source_array[counter] do
+    new_array.push(source_array[counter] *1)
+    counter += 1
+  end
+  return new_array
+end
+
+def map_to_double(source_array)
+  counter = 0
+  new_array = [ ]
+  while source_array[counter] do
+    new_array.push(source_array[counter] *2)
+    counter += 1
+  end
+  return new_array
+end
+
+def map_to_square(source_array)
+  counter = 0
+  new_array = [ ]
+  while source_array[counter] do
+    new_array.push(source_array[counter] ** 2)
+    counter += 1
+  end
+  return new_array
+end
+
+def reduce_to_total(source_array)
+  total = 0
+  i = 0
+  while i < source_array.count
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == false
+      false
+    end
+    i += 1
+  end
+  true
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.size do
+    if source_array == false
+      return true
+    else 
+      return false
+    end
+    i += 1
+  end
+end
+
+def reduce_to_any_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end
