3e46e6af673a10916258d1b67fc04c00f875a45f
munroe1786 <era3munroe@gmail.com>
2019-10-07 11:32:42 -0400

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,67 @@
-# My Code here....
+def map_to_negativize(array)
+    new_array = []
+    counter = 0
+    while counter < array.size do
+        new_array << (array [counter] * -1)
+        counter += 1
+    end
+    new_array
+end
+
+def map_to_no_change(array)
+    new_array = []
+    counter = 0
+    while counter < array.size do
+        new_array << (array [counter] * 1)
+        counter += 1
+    end
+    new_array
+end
+
+def map_to_double(array)
+    new_array = []
+    counter = 0
+    while counter < array.size do
+        new_array << (array [counter] * 2)
+        counter += 1
+    end
+    new_array
+end
+
+def map_to_square(array)
+    new_array = []
+    counter = 0
+    while counter < array.size do
+        new_array << (array [counter] ** 2)
+        counter += 1
+    end
+    new_array
+end
+
+def reduce_to_total(array, starting_point = 0)
+    total = starting_point
+    counter = 0
+    while counter < array.size do
+        total += array[counter]
+        counter += 1
+    end
+    total
+end
+
+def reduce_to_all_true(array)
+    counter = 0
+    while counter < array.size do
+        return true if array.all? == true
+        counter += 1
+    end
+    return false
+end
+
+def reduce_to_any_true(array)
+    counter = 0
+    while counter < array.size do
+        return true if array.any? == true
+        counter += 1
+    end
+    return false
+end
